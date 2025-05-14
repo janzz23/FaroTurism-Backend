132aadc033789ee8db2f21de6f61883824419bef
Tutancamon <cardenasdylan58@gmail.com>
2025-05-13 20:35:46 -0500

    modelo de turista

---
+++ src/models/turista.js
@@ -0,0 +1,34 @@
+const { default: mongoose } = require("mongoose");
+const moongose = require("mongoose");
+
+const turistaSchema = new mongoose.Schema({
+    nombre: { type: String, required: true },
+    apellido: { type: String, required: true },
+    numeroTelefono: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    idiomasHablados: {
+        type: String,
+        required: true,
+        enum: [
+            "Ingles",
+            "Español",
+            "Portugues ",
+            "Frances",
+            "Aleman",
+            "Mandarin",
+            "Japones"
+        ]
+    },
+    Nacionalidad: {
+        type: String,
+        required: true,
+
+    }
+
+
+}, {
+    timestamps: true,
+})
+
+module.exports = mongoose.model("turista", turistaSchema);
